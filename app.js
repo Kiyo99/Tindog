b2e49bde6168573b7952cfd220d8d5d600d14bfa
godsfavourkio <kiofavour@gmail.com>
2022-12-05 14:35:41 +0000

    Added new endpoints

--- app.js
+++ app.js
@@ -11,14 +11,34 @@ app.get("/", function (req, res) {
 });
 
 app.post("/apple", function (req, res) {
-    res.send("<h1>Waiting to download apple app...")
+    // res.send("<h1>Waiting to download apple app...")
+    res.json({
+        "Operating system": "ios",
+        "User": req.ip
+    });
 });
 
 app.post("/play", function (req, res) {
-    res.send("<h1>Waiting to download Google play app...")
+    res.json({
+        "Operating system": "Android"
+    });
+});
+
+app.get("/user/:id", function (req, res) {
+    if (req.params.id == 7) {
+        res.json({
+            "User": "Kio",
+            "Status": true,
+            "color": `${req.query.color}`
+        });
+    }
+    else {
+        res.send(`You entered Id of ${req.params.id}`);
+    }
 });
 
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server running on port 3000");
 });
+
