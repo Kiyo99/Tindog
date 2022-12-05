ca3465e5bf05b41ecaa3d6384f97bbcf7f5ed332
godsfavourkio <kiofavour@gmail.com>
2022-12-05 13:06:14 +0000

    Fixed white space by the side.

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+    res.send("<h1>Waiting to download...")
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server running on port 3000");
