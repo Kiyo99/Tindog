224316e081ad41c036741bb200cb12cc60fb3a30
godsfavourkio <kiofavour@gmail.com>
2022-12-05 11:46:59 +0000

    Updated views

--- app.js
+++ app.js
@@ -6,25 +6,11 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server running on port 3000");
 });
-
-/**
- * 2:15 take down shit and buy soaps and drink
- * 2:30 to 3: Wash freezer
- * 3 - 4:30pm - Wash clothes for tomorrow, Clean
- * 4:45 go and bathe
- * 5:15 go to school
- * 
- * 5:30pm start looking at Kaleb's project again - Picture upload, other features
- * 7pm start Theo's project 
- * 7 to 8 Theo
- * 8 to 9 Documents
- * 9pm start heading back
- */
